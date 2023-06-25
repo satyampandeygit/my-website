c1254126127a09d4b4446744d0b339afbcd1e521
psatyam360 <psatyam360@gmail.com>
2023-06-25 18:57:04 +0530

    repo context used

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Header } from "./Components/Header";
 import { Body } from "./Components/Body";
 import { BrowserRouter } from "react-router-dom";
+import RepoContext from "./Utils/RepoContext";
 
 export const App = () => {
+
+    const [repos, setRepos] = useState([]);
+
+    const showTheseRepos = ['Bill-App', 'bill-app-backend', 'Minsweeper-Project', 'my-website', 'namaste-react', 'xharktank']
+
+    async function fetchRepoDetails(){
+
+        let requiredData = [];
+
+        const response = await fetch('https://api.github.com/users/satyampandeygit/repos');
+            
+        const repoData = await response.json();
+
+        repoData.forEach(repo => {
+            if(repo.private===false && repo.fork===false && repo.language && showTheseRepos.includes(repo.name)){
+                let newObject = {
+                    name: repo?.name,
+                    url: repo?.html_url,
+                    description: repo?.description,
+                    language: repo?.language
+                };
+                requiredData.push(newObject);
+            }
+        });
+
+        setRepos(requiredData);
+    }
+
+    useEffect(() =>{
+        fetchRepoDetails();
+    },[]);
+
     return(
         <React.Fragment>
             <BrowserRouter>
-                <div style={{
-                    // backgroundColor: 'aqua',
-                    height: '1000px',
+                <RepoContext.Provider value={{
+                    reposList: repos
                 }}>
-                    <Header/>
-                    <Body/>
-                </div>
+                    <div style={{
+                        // backgroundColor: 'aqua',
+                        height: '1000px',
+                    }}>
+                        <Header/>
+                        <Body/>
+                    </div>
+                </RepoContext.Provider>
             </BrowserRouter>
         </React.Fragment>
     )
